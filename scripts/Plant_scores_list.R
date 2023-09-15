9f96819161a6d44fb7f9af5e20bb19df924a090a
Jane Ogilvie <janeeogilvie@gmail.com>
2023-09-15 16:37:36 +1000

    Updating the script and data files.

--- scripts/Plant_scores_list.R
+++ scripts/Plant_scores_list.R
@@ -29,7 +29,7 @@ library(tidyverse)
 library(APCalign)
 
 ## Load the list of plant names from AusInverTraits.
-p1 <- read_csv(file.path(getwd(), "data", "plant_scores/plant_names_Aug_2023.csv"), show_col_types = FALSE)
+p1 <- read_csv(file.path(getwd(), "data", "plant_scores/plant_names_13Sep2023.csv"), show_col_types = FALSE)
 
 ## This plant list was extracted from the database with the following code:
 # plant <- ausinverts$contexts %>% 
@@ -38,7 +38,7 @@ p1 <- read_csv(file.path(getwd(), "data", "plant_scores/plant_names_Aug_2023.csv
 #   dplyr::arrange(stringr::str_squish(value)) %>% # sort plant names alphabetically and remove white space
 #   dplyr::distinct(value)  %>% # keep only unique plant names
 #   dplyr::rename(species = value)  %>% # rename the name variable
-#   readr::write_csv("plant_names_Aug_2023.csv") # save plant name list
+#   readr::write_csv("plant_names_13Sep2023.csv") # save plant name list
 
 ## Load the plant scoring data file derived from AusTraits by Sophie Yang.
 p2 <- read_csv(file.path(getwd(), "data", "plant_scores/SCORE_plant_recovery_traits_updated_sep_2023.csv"), show_col_types = FALSE)
@@ -53,15 +53,15 @@ p2 <- read_csv(file.path(getwd(), "data", "plant_scores/SCORE_plant_recovery_tra
 resources <- load_taxonomic_resources()
 
 ## Query our plant names against the taxonomic resources
-updated_names <- p1 %>% 
+standardised_names <- p1 %>% 
   pull(species) %>% 
   create_taxonomic_update_lookup(resources = resources)
 
 ## View the standardised names data
-updated_names %>% View
+standardised_names %>% View
 
 ## Save the updated and standardised plant names to csv.
-readr::write_csv(updated_names, file.path(out_dir, "updated_associated_plant_names_Sep_2023.csv"))
+readr::write_csv(standardised_names, file.path(out_dir, "updated_associated_plant_names_14Sep2023.csv"))
 
 ## -- Manual name changes needed -- ##
 
@@ -82,7 +82,7 @@ readr::write_csv(updated_names, file.path(out_dir, "updated_associated_plant_nam
 # Stipa - not a current genus
 # Bignonia - not found in Australia (I think)
 
-# -- Notes --
+## -- NOTES -- ##
 # The plant family names can't be used because there are not plant scores at this level
 # We need to decide when to implement code that will update plant names in the database
 
@@ -92,21 +92,23 @@ readr::write_csv(updated_names, file.path(out_dir, "updated_associated_plant_nam
 ## list of missing names, create scores data file ###
 ##-------------------------------------------------##
 
-## Join the updated names to the plant scores data file
-plant_names <- updated_names %>% 
-  dplyr::select(original_name, accepted_name, genus) %>% # select only relevant columns
-  dplyr::mutate(updated_name = dplyr::coalesce(accepted_name, genus))  %>% # replace NAs in the accepted_name column, which exist for the genera-only names, with names from the genus column
-  dplyr::left_join(p2, by = join_by(updated_name == taxon_name)) # merge the plant score data file with the updated names
+## Join the updated names to the plant scores data file & create
+## a data set of scores of associated plants in AusInverTraits
+plant_names <- standardised_names %>% 
+  dplyr::select(original_name, accepted_name, genus, update_reason) %>% # select only relevant columns
+  dplyr::mutate(updated_name = dplyr::coalesce(accepted_name, genus)) %>% # replace NAs in the accepted_name column, which exist for the genera-only names, with names from the genus column
+  dplyr::distinct(updated_name, .keep_all = TRUE) %>% # keep only unique plant names
+  dplyr::left_join(p2, by = join_by(updated_name == taxon_name)) %>% # merge the plant score data file with the updated names
+  dplyr::filter(!is.na(score_GM)) %>% # filter rows/plants with values for geometric mean score
+  dplyr::select(-original_name, -accepted_name, -genus, -update_reason) %>% # remove columns not needed
+  readr::write_csv(file.path(out_dir, "invertraits_plant_recovery_scores_15Sep2023.csv")) 
 
 ## Create list of plant names not in plant scores data file
-missing_names <- plant_names %>%
+missing_names <- standardised_names %>%
+  dplyr::select(original_name, accepted_name, genus, update_reason) %>% # select only relevant columns
+  dplyr::mutate(updated_name = dplyr::coalesce(accepted_name, genus)) %>% # replace NAs in the accepted_name column, which exist for the genera-only names, with names from the genus column
+  dplyr::distinct(updated_name, .keep_all = TRUE) %>% # keep only unique plant names
+  dplyr::left_join(p2, by = join_by(updated_name == taxon_name)) %>% # merge the plant score data file with the updated names
   dplyr::filter(!is.na(updated_name)) %>% # remove NAs in name column (these were family or not current genus names only)
   dplyr::filter(is.na(score_GM)) %>% # keep rows with NA in the score column (i.e., those that have no score data)
-  dplyr::distinct(updated_name) %>% # keep only unique plant names
   readr::write_csv(file.path(out_dir, "missing_plant_names_Sep_2023.csv")) # save plant name list
-
-## Create a data set of plant scores of associated plants in AusInverTraits
-plant_score <- plant_names %>%
-  dplyr::filter(!is.na(score_GM)) %>% # filter rows/plants with values for geometric mean score
-  dplyr::select(-accepted_name, -genus) %>% # remove unnecessary columns
-  readr::write_csv(file.path(out_dir, "invertraits_plant_scores_14Sep2023.csv")) # save plant name list
