34b3f1c196f9a7dc8cc9bebabd07b9dcab0be28b
ehwenk <ehwenk@gmail.com>
2025-03-05 11:08:26 +1100

    finalise matching function
    - finalise scripts to create joint AFD, iNat, gbif taxon list. Using gbif for synonyms of names on other lists and for stray names that appear in AusInverTraits but not in other lists
    - finalise script with actual matching function
    - I have a merged taxon list for future reference but too big for GitHub...

--- scripts/create_merged_taxonomic_datasets.R
+++ scripts/create_merged_taxonomic_datasets.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+afd <- read_csv("~/GitHub/ausinvertraits.addons/data/afd_Feb2025.csv")
 
 # thanks to ChatGPT for this function
 split_synonym <- function(synonym) {
@@ -16,53 +17,265 @@ split_synonym <- function(synonym) {
   }
 }
 
-afd_exact <- afd %>%
-  select(accepted_name = FULL_NAME, canonical_name = FULL_NAME, accepted_name_usage_id = CONCEPT_GUID, taxon_id = CONCEPT_GUID, family = FAMILY)
+# create AFD list
+afd_Feb2025 <- read_csv("~/GitHub/ausinvertraits.addons/data/afd_Feb2025.csv")
 
-afd_synonyms <- afd %>%
-  select(accepted_name = FULL_NAME, synonym = SYNONYMS, accepted_name_usage_id = CONCEPT_GUID, family = FAMILY) %>%
-  tidyr::separate_longer_delim(synonym, delim = "; ") %>%
-  rowwise() %>%
+names(afd_Feb2025)
+
+afd_Feb2025_edited <- afd_Feb2025 %>%
   mutate(
-    split = list(split_synonym(synonym))
+    across(3:19, stringr::str_to_sentence)
   ) %>%
-  unnest_wider(split, names_sep = "_") %>%
-  rename(canonical_name = split_1, author_year = split_2) %>%
   mutate(
-    canonical_name = str_trim(canonical_name),
-    taxon_id = ifelse(accepted_name == canonical_name, accepted_name_usage_id, paste0(accepted_name_usage_id, "_", synonym))
-    )
-  
-afd_all <- afd_exact %>%  
-  bind_rows(afd_synonyms %>% select(-synonym, -author_year)) %>%
-  distinct() %>%
-  mutate(
-    canonical_name = ifelse(is.na(canonical_name), accepted_name, canonical_name),
-    taxon_id = ifelse(is.na(taxon_id), accepted_name_usage_id, taxon_id),
-    taxonomic_status = ifelse(accepted_name_usage_id == taxon_id, "accepted", "taxonomic synonym"),
-    taxon_rank = ifelse(stringr::str_count(canonical_name, " ") == 1, "species", "subspecies"),
-    genus = stringr::word(accepted_name, start = 1),
-    family = stringr::str_to_sentence(family)
-  )
-  
-
-afd_all %>% write_csv("config/taxon_lists/afd_tweaked.csv")  
-
-
-# version 2024-02-04
-
-afd_final <- afd_final %>% 
-  rename(genus = GENUS, family = FAMILY, order = ORDER, class = CLASS, phylum = PHYLUM)
-
-  
-# 2025-01-23 adding additional taxonomic datasets
-
-afd_inverts <- read_csv("config/taxon_lists/afd_tweaked.csv") %>%
-  mutate(
-    taxonomic_status = stringr::str_replace(taxonomic_status, "taxonomic synonym", "synonym")
+    taxon_rank = ifelse(is.na(SUB_SPECIES), "species", "subspecies"),
+    SUBSPECIES_COUNT_IN_SPECIES = as.character(SUBSPECIES_COUNT_IN_SPECIES),
+    taxon_name = FULL_NAME
   )
 
-names(afd_inverts)
+subgenera <- afd_Feb2025_edited %>%
+  distinct(SUB_GENUS, .keep_all = T) %>%
+  filter(!is.na(SUB_GENUS)) %>%
+  mutate(
+    across(20:38, ~ NA_character_),
+    taxon_rank = "subgenus",
+    taxon_name = SUB_GENUS
+  )
+
+genera <- afd_Feb2025_edited %>%
+  distinct(GENUS, .keep_all = T) %>%
+  filter(!is.na(GENUS)) %>%
+  mutate(
+    across(19:38, ~ NA_character_),
+    taxon_rank = "genus",
+    taxon_name = GENUS
+  )
+
+subtribe <- afd_Feb2025_edited %>%
+  distinct(SUBTRIBE, .keep_all = T) %>%
+  filter(!is.na(SUBTRIBE)) %>%
+  mutate(
+    across(18:38, ~ NA_character_),
+    taxon_rank = "subtribe",
+    taxon_name = SUBTRIBE
+  )
+
+tribe <- afd_Feb2025_edited %>%
+  distinct(TRIBE, .keep_all = T) %>%
+  filter(!is.na(TRIBE)) %>%
+  mutate(
+    across(17:38, ~ NA_character_),
+    taxon_rank = "tribe",
+    taxon_name = TRIBE
+  )
+
+supertribe <- afd_Feb2025_edited %>%
+  distinct(SUPERTRIBE, .keep_all = T) %>%
+  filter(!is.na(SUPERTRIBE)) %>%
+  mutate(
+    across(16:38, ~ NA_character_),
+    taxon_rank = "supertribe",
+    taxon_name = SUPERTRIBE
+  )
+
+subfamily <- afd_Feb2025_edited %>%
+  distinct(SUBFAMILY, .keep_all = T) %>%
+  filter(!is.na(SUBFAMILY)) %>%
+  mutate(
+    across(15:38, ~ NA_character_),
+    taxon_rank = "subfamily",
+    taxon_name = SUBFAMILY
+  )
+
+family <- afd_Feb2025_edited %>%
+  distinct(FAMILY, .keep_all = T) %>%
+  filter(!is.na(FAMILY)) %>%
+  mutate(
+    across(14:38, ~ NA_character_),
+    taxon_rank = "family",
+    taxon_name = FAMILY
+  )
+
+superfamily <- afd_Feb2025_edited %>%
+  distinct(SUPERFAMILY, .keep_all = T) %>%
+  filter(!is.na(SUPERFAMILY)) %>%
+  mutate(
+    across(13:38, ~ NA_character_),
+    taxon_rank = "superfamily",
+    taxon_name = SUPERFAMILY
+  )
+
+suborder <- afd_Feb2025_edited %>%
+  distinct(SUBORDER, .keep_all = T) %>%
+  filter(!is.na(SUBORDER)) %>%
+  mutate(
+    across(12:38, ~ NA_character_),
+    taxon_rank = "suborder",
+    taxon_name = SUBORDER
+  )
+
+order <- afd_Feb2025_edited %>%
+  distinct(ORDER, .keep_all = T) %>%
+  filter(!is.na(ORDER)) %>%
+  mutate(
+    across(11:38, ~ NA_character_),
+    taxon_rank = "order",
+    taxon_name = ORDER
+  )
+
+superorder <- afd_Feb2025_edited %>%
+  distinct(SUPERORDER, .keep_all = T) %>%
+  filter(!is.na(SUPERORDER)) %>%
+  mutate(
+    across(10:38, ~ NA_character_),
+    taxon_rank = "superorder",
+    taxon_name = SUPERORDER
+  )
+
+subclass <- afd_Feb2025_edited %>%
+  distinct(SUBCLASS, .keep_all = T) %>%
+  filter(!is.na(SUBCLASS)) %>%
+  mutate(
+    across(9:38, ~ NA_character_),
+    taxon_rank = "subclass",
+    taxon_name = SUBCLASS
+  )
+
+class <- afd_Feb2025_edited %>%
+  distinct(CLASS, .keep_all = T) %>%
+  filter(!is.na(CLASS)) %>%
+  mutate(
+    across(8:38, ~ NA_character_),
+    taxon_rank = "class",
+    taxon_name = CLASS
+  )
+
+superclass <- afd_Feb2025_edited %>%
+  distinct(SUPERCLASS, .keep_all = T) %>%
+  filter(!is.na(SUPERCLASS)) %>%
+  mutate(
+    across(7:38, ~ NA_character_),
+    taxon_rank = "superclass",
+    taxon_name = SUPERCLASS
+  )
+
+subphylum <- afd_Feb2025_edited %>%
+  distinct(SUBPHYLUM, .keep_all = T) %>%
+  filter(!is.na(SUBPHYLUM)) %>%
+  mutate(
+    across(6:38, ~ NA_character_),
+    taxon_rank = "subphylum",
+    taxon_name = SUBPHYLUM
+  )
+
+phylum <- afd_Feb2025_edited %>%
+  distinct(PHYLUM, .keep_all = T) %>%
+  filter(!is.na(PHYLUM)) %>%
+  mutate(
+    across(5:38, ~ NA_character_),
+    taxon_rank = "phylum",
+    taxon_name = PHYLUM
+  )
+
+# Merge together the dataframes for all the different taxon ranks
+afd_accepted <- afd_Feb2025_edited %>%
+  bind_rows(subgenera, genera, subtribe, tribe, supertribe, subfamily, family, superfamily, suborder, order, superorder, subclass, class, superclass, subphylum, phylum) %>%
+  mutate(
+    taxonomic_status = "accepted",
+    taxonomic_dataset = "AFD",
+    canonical_name = taxon_name,
+    accepted_name = taxon_name,
+    accepted_name_usage_id = ifelse(!is.na(CONCEPT_GUID), CONCEPT_GUID, taxon_name),
+    taxon_id = ifelse(!is.na(CONCEPT_GUID), CONCEPT_GUID, taxon_name),
+  ) %>%
+  select(taxon_name, canonical_name, accepted_name, taxon_rank, taxonomic_status, taxonomic_dataset, accepted_name_usage_id, taxon_id, genus = GENUS, family = FAMILY, order = ORDER, 
+         class = CLASS, phylum = PHYLUM, subtribe = SUBTRIBE, subgenus = SUB_GENUS, tribe = TRIBE, supertribe = SUPERTRIBE, subfamily = SUBFAMILY, superfamily = SUPERFAMILY, everything()) %>%
+  mutate(scientific_name = NA)
+
+
+# Function to remove author names from synonyms - any uppercase word that is not at the beginning is removed
+remove_uppercase <- function(x) {
+  x <- str_replace(x, " d'", " ")
+  
+  # Split the string into words
+  words <- strsplit(x, " ")[[1]]
+  
+  # Keep the first word and remove subsequent ones that start with uppercase letters
+  result <- c(words[1], words[-1][!grepl("^[A-Z]", words[-1])])
+  
+  # Recombine the string
+  result <- paste(result, collapse = " ")
+  
+  # Remove stray information
+  result <- str_replace(result, " &", "")
+  result <- str_replace(result, " in ", " ")
+  result <- str_replace(result, " de ", " ")
+  result <- str_trim(result)
+  
+  return(result)
+}
+
+# Test the above function on diverse names
+names_to_test <- c("Abracadbrella lewiston Zabka", "Onychoteuthis armata Quoy & Gaimard",  "Chooreechillum distitans T.P. Lucas", "Cladorhiza pentaeiros Ekins, Erpenbeck & Hooper", 
+                   "Lamia fasciata Montrouzier", "Dihammus fasciatus aurivarius Gressitt", "Acanthochites (Meturoplax) retrojectus Pilsbry")
+
+modified_names <- sapply(names_to_test, remove_uppercase)
+
+
+# Assemble table of synonyms
+afd_Feb2025_synonyms <- afd_Feb2025_edited %>%
+  tidyr::separate_longer_delim(SYNONYMS, delim = "; ") %>% 
+  mutate(
+    synonym_compare = paste0(FULL_NAME, " ", AUTHOR, ", ", YEAR) 
+  ) %>%
+  filter(SYNONYMS != synonym_compare) %>% 
+  mutate(
+    AUTHOR = stringr::str_replace(AUTHOR, "\\(",""),
+    AUTHOR = stringr::str_replace(AUTHOR, "\\)",""),
+    taxon_name_tmp = stringr::str_replace(SYNONYMS, "\\,\\s[:digit:][:digit:][:digit:][:digit:]", ""),
+    taxon_name_no_author = NA,
+    taxon_name_no_author2 = NA,
+    taxonomic_status = "synonym",
+    taxonomic_dataset = "AFD",
+    accepted_name = taxon_name
+  )
+
+# Remove authors that are detected in the author list
+authors <- afd_Feb2025 %>% 
+  distinct(AUTHOR) %>%
+  mutate(AUTHOR = paste0(AUTHOR)) %>% 
+  pull(AUTHOR) %>%
+  gsub("([[:punct:]])", "\\\\\\1", .)
+
+regex_authors <- paste(authors, collapse = "$|")
+
+i <-
+  str_detect(afd_Feb2025_synonyms$taxon_name_tmp, regex_authors)
+
+afd_Feb2025_synonyms$taxon_name_no_author2 <- str_replace(afd_Feb2025_synonyms$taxon_name_tmp, regex_authors, "")
+
+
+# Further remove any upper case words that aren't at the beginning of the name
+for (j in 1:nrow(afd_Feb2025_synonyms)) {
+  afd_Feb2025_synonyms$taxon_name_no_author[[j]] <- remove_uppercase(afd_Feb2025_synonyms$taxon_name_no_author2[[j]])
+}
+
+# Reorder columns for checking
+afd_synonyms <- afd_Feb2025_synonyms %>%
+  mutate(
+    canonical_name = str_trim(taxon_name_no_author),
+    accepted_name_usage_id = ifelse(!is.na(CONCEPT_GUID), CONCEPT_GUID, canonical_name),
+    taxon_id = ifelse(!is.na(CONCEPT_GUID), paste0(CONCEPT_GUID,"_",canonical_name), paste0(accepted_name,"_",canonical_name)),
+  ) %>%
+  select(-taxon_name_no_author2, -taxon_name_no_author, -taxon_name_tmp, -synonym_compare) %>%
+  select(taxon_name, canonical_name, accepted_name, scientific_name = SYNONYMS, taxon_rank, taxonomic_status, taxonomic_dataset, accepted_name_usage_id, taxon_id, genus = GENUS, family = FAMILY, order = ORDER, 
+         class = CLASS, phylum = PHYLUM, subtribe = SUBTRIBE, subgenus = SUB_GENUS, tribe = TRIBE, supertribe = SUPERTRIBE, subfamily = SUBFAMILY, superfamily = SUPERFAMILY,
+         everything()) 
+
+afd_synonyms <- afd_Feb2025_synonyms 
+
+afd_inverts <- afd_accepted %>% bind_rows(afd_synonyms)
+
+write_csv(afd_inverts, "config/taxon_lists/AFD_inverts_20250305.csv")
 
 iNat_inverts_in_Australia <- read_csv("~/GitHub/ausinvertraits.addons/data/iNat_inverts_in_Australia.csv")
 
@@ -83,11 +296,11 @@ iNat_inverts <- iNat_inverts_in_Australia %>%
     taxonomic_dataset = "iNaturalist"
   )
 
-taxon_list <- afd_final %>%
+taxon_list <- afd_inverts %>%
   bind_rows(iNat_inverts) %>%
   mutate(
     taxonomic_dataset = factor(taxonomic_dataset, levels = c("AFD", "iNaturalist")),
-    scientific_name = FULL_NAME,
+    ,
   ) %>%
   dplyr::arrange(taxonomic_dataset, canonical_name) %>%
   dplyr::group_by(canonical_name) %>%
@@ -97,38 +310,110 @@ taxon_list <- afd_final %>%
   dplyr::ungroup() %>%
   dplyr::distinct(canonical_name, .keep_all = TRUE)
 
-# add columns before splitting
-zzz <- "zzzz zzzz"
+# add synonyms from gbif we don't have
+
+gbif_taxa <- read_tsv("~/GitHub/ausinvertraits.addons/GBIF_taxonomy/Taxon.tsv")
+
+gbif_taxa <- gbif_taxa %>% 
+  arrange(taxonomicStatus) %>%
+  group_by(canonicalName) %>%
+  slice(1) %>%
+  ungroup()
+
+gbif_accepted_in_other <- gbif_taxa %>%
+  filter(canonicalName %in% taxon_list$canonical_name, taxonomicStatus == "accepted")
+
+other_not_in_gbif <- taxon_list %>%
+  filter(!canonical_name %in% gbif_taxa$canonicalName)
+
+synonyms_to_add_from_gbif <-
+  gbif_taxa %>%
+  filter(acceptedNameUsageID %in% gbif_accepted_in_other$taxonID) %>%
+  filter(!canonicalName %in% taxon_list$canonical_name) %>%
+  mutate(
+    accepted_name = gbif_taxa$canonicalName[match(acceptedNameUsageID, gbif_taxa$taxonID)],
+    taxonomic_dataset = "gbif"
+  ) %>%
+  select(accepted_name, canonical_name = canonicalName, accepted_name_usage_id = acceptedNameUsageID, taxon_id = taxonID, genus, family, order, class, phylum, scientific_name = scientificName, taxonomic_status = taxonomicStatus, taxon_rank = taxonRank, taxonomic_dataset)
+  
+add_to_invert_reference_list <- read_csv("~/GitHub/ausinvertraits.addons/data/add_to_invert_reference_list.csv")
+
+iNat_taxonomy <- read_csv("~/iNat_taxonomy.csv")
+
+# this section is picking up taxon names that were unmatched in early rounds of testing, because 1) they aren't in AFD; 2) don't have any observations in iNat in Australia
+# it is small numbers of taxa to add
+
+add_to_invert_reference_list %>% 
+  select(-genus, -taxonomic_status, - valid_name) %>% 
+  left_join(iNat_taxonomy %>% rename(taxon_name = scientificName)) %>% 
+  filter(!is.na(taxonID)) %>%
+  rename(taxon_id = taxonID, taxon_rank = taxonRank, canonical_name = taxon_name) %>%
+  mutate(
+    accepted_name_usage_id = taxon_id,
+    taxonomic_status = "accepted",
+    taxonomic_dataset = "iNaturalist",
+    scientific_name = NA,
+    accepted_name = canonical_name
+    ) %>%
+  select(any_of(names(taxon_list))) -> add_to_invert_reference_list_match1
+
+add_to_invert_reference_list %>% 
+  filter(!taxon_name %in% add_to_invert_reference_list_match1$canonical_name) %>% 
+  select(-genus, -taxonomic_status, -valid_name) %>% 
+  left_join(gbif_taxa %>% rename(taxon_name = canonicalName)) %>% 
+  filter(!is.na(taxonID)) %>%
+  rename(taxon_id = taxonID, taxon_rank = taxonRank, scientific_name = scientificName, 
+         taxonomic_status = taxonomicStatus, accepted_name_usage_id = acceptedNameUsageID,
+         canonical_name = taxon_name) %>%
+  mutate(
+    accepted_name_usage_id = ifelse(is.na(accepted_name_usage_id), taxon_id, accepted_name_usage_id),
+    accepted_name_usage_id = as.character(accepted_name_usage_id),
+    taxon_id = as.character(taxon_id),
+    taxonomic_dataset = "gbif",
+    accepted_name = gbif_taxa$canonicalName[match(accepted_name_usage_id, gbif_taxa$taxonID)]
+    ) %>%
+  select(any_of(names(taxon_list))) -> add_to_invert_reference_list_match2
+
+add_to_invert_reference_list %>% 
+  filter(!taxon_name %in% add_to_invert_reference_list_match1$canonical_name) %>% 
+  filter(!taxon_name %in% add_to_invert_reference_list_match2$canonical_name) %>% 
+  select(-genus, -taxonomic_status) %>% 
+  left_join(gbif_taxa %>% rename(valid_name = canonicalName)) %>% 
+  filter(!is.na(taxonID)) %>%
+  rename(taxon_id = taxonID, taxon_rank = taxonRank, scientific_name = scientificName, 
+         taxonomic_status = taxonomicStatus, accepted_name_usage_id = acceptedNameUsageID,
+         canonical_name = valid_name) %>%
+  mutate(
+    accepted_name_usage_id = ifelse(is.na(accepted_name_usage_id), taxon_id, accepted_name_usage_id),
+    accepted_name_usage_id = as.character(accepted_name_usage_id),
+    taxon_id = as.character(taxon_id),
+    taxonomic_dataset = "gbif",
+    accepted_name = gbif_taxa$canonicalName[match(accepted_name_usage_id, gbif_taxa$taxonID)]
+    ) %>%
+  select(any_of(names(taxon_list))) -> add_to_invert_reference_list_match3
+
+# another list based on unmatched names checked against gbif - March 5, 2025!
+also_add %>%
+  rename(taxon_id = taxonID, taxon_rank = taxonRank, scientific_name = scientificName, 
+         taxonomic_status = taxonomicStatus, accepted_name_usage_id = acceptedNameUsageID,
+         canonical_name = canonicalName) %>%
+  mutate(
+    accepted_name_usage_id = ifelse(is.na(accepted_name_usage_id), taxon_id, accepted_name_usage_id),
+    accepted_name_usage_id = as.character(accepted_name_usage_id),
+    taxon_id = as.character(taxon_id),
+    taxonomic_dataset = "gbif",
+    accepted_name = gbif_taxa$canonicalName[match(accepted_name_usage_id, gbif_taxa$taxonID)]
+  ) %>%
+  select(any_of(names(taxon_list))) -> add_to_invert_reference_list_match4
 
 taxon_list <- taxon_list %>%
-    dplyr::mutate(
-      word_one = extract_genus(canonical_name),
-      taxon_rank = standardise_taxon_rank(taxon_rank),
-      taxon_rank2 = ifelse(taxon_rank %in% c("subspecies", "species", "form", "variety"), "species", taxon_rank),
-      ## strip_names removes punctuation and filler words associated with
-      ## infraspecific taxa (subsp, var, f, ser)
-      stripped_canonical = strip_names(canonical_name),
-      ## strip_names_extra removes extra filler words associated with 
-      ## species name cases (x, sp)
-      ## strip_names_extra is essential for the matches involving 2 or 3 words,
-      ## since you want those words to not count filler words
-      stripped_canonical2 = strip_names_extra(stripped_canonical),
-      stripped_scientific = strip_names(scientific_name),
-      binomial = ifelse(
-        taxon_rank == "species",
-        word(stripped_canonical2, start = 1, end = 2),
-        zzz
-      ),
-      binomial = ifelse(is.na(binomial), zzz, binomial),
-      binomial = base::replace(binomial, duplicated(binomial), zzz),
-      genus = extract_genus(stripped_canonical),
-      trinomial = word(stripped_canonical2, start = 1, end = 3),
-      trinomial = ifelse(is.na(trinomial), zzz, trinomial),
-      trinomial = base::replace(trinomial, duplicated(trinomial), zzz)
-    )
-  
-resources <- taxon_list %>% split(taxon_list$taxon_rank2)
+  bind_rows(synonyms_to_add_from_gbif %>% 
+              mutate(accepted_name_usage_id = as.character(accepted_name_usage_id),
+                     taxon_id = as.character(taxon_id))
+            ) %>%
+  bind_rows(add_to_invert_reference_list_match1) %>%
+  bind_rows(add_to_invert_reference_list_match2) %>% 
+  bind_rows(add_to_invert_reference_list_match3) %>% 
+  bind_rows(add_to_invert_reference_list_match4)
 
-resources$species <- resources$species %>% split(resources$species$taxonomic_status)
-
-taxon_ranks_to_check <- c("subgenus", "genus", "family", "superfamily", "subclass", "class", "subfamily", "order", "infraorder", "suborder", "superorder", "tribe")
+write_csv(taxon_list, "~/GitHub/ausinvertraits.addons/data/taxon_list_for_AusInvertAlign.csv", na = "")
